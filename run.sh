7743b0a887564f8bbb2685da35743bfadcc5c27b
Thomas Deutsch <thomas@tuxpeople.org>
2020-12-10 21:58:37 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 CWD=`pwd`
 cd `dirname "$(realpath $0)"`
 
+curl -L https://raw.githubusercontent.com/tuxpeople/dotfiles/master/Brewfile > files/Brewfile
+
+ansible-galaxy install -r requirements.yml
+
 ansible-playbook main.yml -i inventory -K
 
 cd $CWD
