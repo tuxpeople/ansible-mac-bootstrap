6662d557b07a8d1d46efa3a89ea0c89f7035b9f3
Thomas Deutsch <thomas@tuxpeople.org>
2021-06-26 14:46:16 +0200

    Update bgimage.sh

--- files/bgimage.sh
+++ files/bgimage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-osascript -e 'tell application "System Events" to tell every desktop to set picture to ~/git/dotfiles/wallpaper/luca-micheli-422053-unsplash.jpg'
+osascript -e 'tell application "System Events" to tell every desktop to set picture to "~/git/dotfiles/wallpaper/luca-micheli-422053-unsplash.jpg"'
